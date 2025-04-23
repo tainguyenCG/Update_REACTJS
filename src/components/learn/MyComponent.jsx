76d9be43332d3f857a5f537a1ae2ff95841893ba
Thanh Tai <thanhtainguyen3923@gmail.com>
2025-04-24 00:01:27 +0700

    {JSON.stringify(DaylaFragment)}

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,14 @@
 const MyComponent = () => {
-  return <h1>Bài học mới của tôi</h1>;
+  const DaylaFragment = "Fragment để bọc tất cả phần tử, gọi là mảnh vỡ ";
+  return (
+    <>
+      <h1>Bài học mới của tôi</h1>
+      <h1>{DaylaFragment}</h1>
+      {/* có thể chuyển chuỗi, mảng, oject bằng cách 
+            {JSON.stringify(DaylaFragment)}
+        */}
+    </>
+  );
 }; // gọi nó ra tại vị trí khác = cách  <MyComponent></MyComponent> hoặc <MyComponent/>
 
 export default MyComponent;
