97a157de1698ea55fa6b25b861f994f7e36aa1e7
Thanh Tai <thanhtainguyen3923@gmail.com>
2025-04-26 15:06:25 +0700

    onChange, onClick

--- src/components/todo/todo_new.jsx
+++ src/components/todo/todo_new.jsx
@@ -3,11 +3,23 @@ const TodoNew = (props) => {
   const {addNewTodo} = props;
 
   // addNewTodo("thanhtai");
+  const handleClick = () =>{
+    alert("Bạn đã thêm thành công")
+  }
+
+  const handleOnChange = () =>{
+    console.log("handleOnChange", event.target.value)
+  }
   return (
     <>
       <div className="todo_new">
-        <input type="text" />
-        <button>Add</button>
+        <input type="text"
+        onChange={(event) => {handleOnChange(event.target.value)}} /> 
+        {/* onChange gõ giá trị vào input */}
+        <button
+        style={{cursor: "pointer"}}
+        onClick={handleClick} //onClick sử dụng chuột để click
+        >Add</button>
       </div>
     </>
   );
